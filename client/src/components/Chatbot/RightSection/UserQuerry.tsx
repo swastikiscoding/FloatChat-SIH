8b3772ae90d60c26a89639aa34b0f7cdf43deb1a
Swastik <swastikiscoding@gmail.com>
2025-09-17 18:47:11 +0530

    refactor: remove unnecessary React imports from components

--- client/src/components/Chatbot/RightSection/UserQuerry.tsx
+++ client/src/components/Chatbot/RightSection/UserQuerry.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 
 const UserQuerry = (  { Text }: { Text: string } ) => {
   return (
--- client/src/components/Chatbot/SideBar/Sidebar.tsx
+++ client/src/components/Chatbot/SideBar/Sidebar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import History from "./History";
 import Modes from "./Modes";
 
