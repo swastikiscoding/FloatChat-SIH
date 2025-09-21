4f6c74dcc36523bae38730ba4059da09886713c5
Swastik <swastikiscoding@gmail.com>
2025-09-21 19:41:29 +0530

    refactor: update PostgreSQL connection configuration to use DATABASE_URL

--- nodejs-server/src/db/postgres.js
+++ nodejs-server/src/db/postgres.js
@@ -1,12 +1,12 @@
 import pkg from 'pg';
 const { Pool } = pkg;
+import dotenv from 'dotenv';
+dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER || 'postgres',
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_NAME || 'argo_data',
-  password: "Swastik@220705",
-  port: 5432, 
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false }, 
 });
+
 
 export default pool;
