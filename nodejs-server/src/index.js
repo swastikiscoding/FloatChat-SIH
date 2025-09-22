e9d4ae0b78e4413366f193859606eb08e67b2da4
Swastik <swastikiscoding@gmail.com>
2025-09-22 19:00:26 +0530

    feat: implement server ping functionality with axios

--- nodejs-server/src/index.js
+++ nodejs-server/src/index.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import { connectDB } from './db/db.js';
 import { app } from './app.js';
+import axios from 'axios';
 
 dotenv.config();
 
@@ -8,6 +9,15 @@ const PORT = process.env.PORT || 3000;
 // app.listen(PORT, () => {
 //     console.log(`Server is running on port ${PORT}`);
 //   });
+const nodeurl = "https://floatchat-sih-69r4.onrender.com";
+const fastapiurl = "https://floatchat-sih-1.onrender.com";
+const interval = 60000 * 8; // 8 minutes in milliseconds
+async function pingServer() {
+    const res = await axios.get(nodeurl);
+    const res2 = await axios.get(fastapiurl);
+}
+setInterval(pingServer, interval);
+pingServer(); 
 
 connectDB()
 .then(() => {
