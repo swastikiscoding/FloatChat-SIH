4bbcc0f6df995c95395116ddb1a15b49f8490d58
Swastik <swastikiscoding@gmail.com>
2025-09-30 10:28:02 +0530

    Fix axios instance baseURL configuration for production environment

--- client/src/utils/axiosInstance.ts
+++ client/src/utils/axiosInstance.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3000/api',
-  // baseURL: 'https://floatchat-sih-69r4.onrender.com/api',
+  // baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://floatchat-sih-69r4.onrender.com/api',
   withCredentials: true,
 });
 
