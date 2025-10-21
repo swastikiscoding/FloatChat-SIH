6e0fe22217b847316a85c8e0cae19b1f3ff8c071
Swastik <swastikiscoding@gmail.com>
2025-10-21 11:14:26 +0530

    Disable server pinging code
    Comment out server pinging functionality for now.

--- nodejs-server/src/index.js
+++ nodejs-server/src/index.js
@@ -9,15 +9,15 @@ const PORT = process.env.PORT || 3000;
 // app.listen(PORT, () => {
 //     console.log(`Server is running on port ${PORT}`);
 //   });
-const nodeurl = "https://floatchat-sih-69r4.onrender.com";
-const fastapiurl = "https://floatchat-sih-1.onrender.com";
-const interval = 60000 * 8; // 8 minutes in milliseconds
-async function pingServer() {
-    const res = await axios.get(nodeurl);
-    const res2 = await axios.get(fastapiurl);
-}
-setInterval(pingServer, interval);
-pingServer(); 
+// const nodeurl = "https://floatchat-sih-69r4.onrender.com";
+// const fastapiurl = "https://floatchat-sih-1.onrender.com";
+// const interval = 60000 * 8; // 8 minutes in milliseconds
+// async function pingServer() {
+//     const res = await axios.get(nodeurl);
+//     const res2 = await axios.get(fastapiurl);
+// }
+// setInterval(pingServer, interval);
+// pingServer(); 
 
 connectDB()
 .then(() => {
